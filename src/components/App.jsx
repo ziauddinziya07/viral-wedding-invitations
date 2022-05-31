691e287ae213822dd08ec53620491b81f11297c8
ziauddinziya07 <ziauddinziya07@gmail.com>
2022-05-31 16:15:05 +0530

    Added auto redirection to Ziya Invites

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import "../styles/App.css";
+function App() {
+    window.location.href = "https://ziya-invites.netlify.app/";
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default App;
+
+
+
+// Our invites are very time-effective. We make it only in 2 to 5 working hours,
+// and you can send it to your all friends and relatives with just one click. 
+// Online wedding invitations are trendy among individuals. This latest trend of 
+// sending cards online is in more demand than any other form. The procedure of 
+// wedding cards is much cheaper than traditional methods. So take away the 
+// stress of wedding invitation. We will make it surprisingly, and you can share
+//  it on social media easily.
+
+// Make an accurate impression on your guest with a beautiful eCard to announce your event. Wow-factor guaranteed!
+// Be Original & Unique
+// It’s not just an invite but an occasion to tell the idea of your love through your text, photos.
+// Be Eco-Friendly: Go Paperless
+// Our eCards are a greener, environmentally friendly alternative to traditional paper stationery. Let’s save the world together!
+// Save Money & Time
+// We have successfully delivered our “Digital Invitation eCards” worldwide and are waiting to deliver to your country also.
+// We make it only within 2-5 working hours, and you can send it to your all friends and relatives with one click.
